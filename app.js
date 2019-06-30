3b5548bcc26ababee6595ff5b414e1ab9862e0f8
Eman-sh <amoooony@hotmail.com>
2019-06-30 19:10:10 +0300

    add restart option

--- app.js
+++ app.js
@@ -5,20 +5,22 @@ $(document).ready(function () {
     let playerOneCount = 1;
     let playerTwoCount = 1;
     let moves = 1;
-    const grid = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "],];
-    const win = [ [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7],] ; // return the winner 
-
+    let grid = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "],];
+    // let gridVal = $(this).index(); 
 
     // 2 players take turns in playing
     const gamePlay = $(".column").one("click", function () {
+        let index = $(this).index(); // gives only [0,1,2]
+        console.log(index)
         let p1;
         let p2;
+
         if (playerOneCount <= playerTwoCount) {
             p1 = $(this).text(playerOne)
+
+
             // Count player moves
             console.log("player1= " + playerOneCount++)
-            if (p1 == win) {
-            console.log("playerOne win")}
 
 
         } else {
@@ -28,24 +30,34 @@ $(document).ready(function () {
 
         }
         // Count Total moves and end game if more than 8
-        if (moves == 9) {
+         if (moves == 9) {
             console.log("Game Over")
         }
         else {
             console.log("moves = " + moves++)
         }
 
+});
+
+    //////////////////////////////////////
+    const winner = function () {
+        const win = [[0, 1, 2], [4, 5, 6], [7, 8, 9],
+        [1, 4, 7], [2, 5, 8], [3, 6, 9],
+        [1, 5, 9], [3, 5, 7],]; // return the winner 
+        for( let i=0;  i <= grid.length; i++) {
+             gris2= grid;
+             console.log(grid)
+    }}
+ 
+
+
+
+// Restart the game 
+    const restartGame = $(".restart").on("click", function () {
+        location.reload();
     });
 
-
-    // let restartGame = function() {
-
-
-
-    // go back to the main function
-
-    // }
-
+});
 
     // for (let i = 0; i<3; i++) {
     //     if (grid[i][0] !== " " && )
@@ -65,4 +77,3 @@ $(document).ready(function () {
 
 
 
-});
