22739bba34e976ba45cac3e7cf69cc759957266f
Eman-sh <amoooony@hotmail.com>
2019-06-30 09:42:48 +0300

    Grid size fix

---
+++ app.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+    let playerOne = "X";
+    let playerTwo = "O";
+    const grid = [
+        [" ", " ", " "],
+        [" ", " ", " "],
+        [" ", " ", " "],
+    ];
+
+    // let gamePlay = function() {
+
+    $(".column").one("click", function () {
+        $(this).text(playerOne)
+
+    });
+    // }
+
+
+    // let players = function() {
+
+    // }
+
+
+
+    // let restartGame = function() {
+
+    // }
+
+
+    // let theWinner = function() {
+
+    // }
+
+
+
+
+});
