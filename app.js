286b78b66cc9fcf61af83dd8cabf9772c106e3ac
Eman-sh <amoooony@hotmail.com>
2019-06-30 23:54:57 +0300

    video page

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 $(document).ready(function () {
-
+//     let grid = [
+//         [" ", " ", " "],
+//         [" ", " ", " "],
+//        [" ", " ", " "]
+//    ];
     let playerOne = "X";
     let playerTwo = "O";
     let playerOneCount = 1;
     let playerTwoCount = 1;
     let moves = 1;
-    let grid = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "],];
-    // let gridVal = $(this).index(); 
+    let players;
+    let p1;
+    let p2;
+
 
     // 2 players take turns in playing
     const gamePlay = $(".column").one("click", function () {
-        let index = $(this).index(); // gives only [0,1,2]
-        console.log(index)
-        let p1;
-        let p2;
+        // players.push($(this).attr('class'));
+
 
         if (playerOneCount <= playerTwoCount) {
             p1 = $(this).text(playerOne)
+      
 
 
             // Count player moves
-            console.log("player1= " + playerOneCount++)
+            console.log("player1 = " + playerOneCount++)
 
 
         } else {
@@ -28,35 +33,39 @@ $(document).ready(function () {
             // Count player moves
             console.log("player2 = " + playerTwoCount++)
 
-        }
+                }
+              
+
         // Count Total moves and end game if more than 8
-         if (moves == 9) {
+        if (moves == 9) {
+            
             console.log("Game Over")
         }
         else {
             console.log("moves = " + moves++)
         }
 
-});
+    });
+    //innerHTML
+    //textContent
 
     //////////////////////////////////////
     const winner = function () {
         const win = [[0, 1, 2], [4, 5, 6], [7, 8, 9],
         [1, 4, 7], [2, 5, 8], [3, 6, 9],
         [1, 5, 9], [3, 5, 7],]; // return the winner 
-        for( let i=0;  i <= grid.length; i++) {
-             gris2= grid;
-             console.log(grid)
-    }}
- 
+
+    }
 
 
 
-// Restart the game 
+
+    // Restart the game 
     const restartGame = $(".restart").on("click", function () {
         location.reload();
     });
 
+    
 });
 
     // for (let i = 0; i<3; i++) {
