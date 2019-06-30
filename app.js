8f1c255f6ae22b58c8d1c7c4eebe2ebf020026a1
Eman-sh <amoooony@hotmail.com>
2019-06-30 14:06:34 +0300

    9 moves = gameover

--- app.js
+++ app.js
@@ -4,74 +4,49 @@ $(document).ready(function () {
     let playerTwo = "O";
     let playerOneCount = 1;
     let playerTwoCount = 1;
-    let moves = 0;
-    const grid = [
-        [" ", " ", " "],
-        [" ", " ", " "],
-        [" ", " ", " "],
-    ];
-    console.log(grid)
+    let moves = 1;
+    const grid = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "],];
+    const win = [ [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7],] ; // return the winner 
 
-    // const gamePlay = function (player1, player2) {
 
+    // 2 players take turns in playing
     const gamePlay = $(".column").one("click", function () {
+        let p1;
+        let p2;
         if (playerOneCount <= playerTwoCount) {
-            $(this).text(playerOne)
-            playerOneCount++
+            p1 = $(this).text(playerOne)
+            // Count player moves
+            console.log("player1= " + playerOneCount++)
+            if (p1 == win) {
+            console.log("playerOne win")}
+
+
         } else {
-            $(this).text(playerTwo)
-            playerTwoCount++
+            p2 = $(this).text(playerTwo)
+            // Count player moves
+            console.log("player2 = " + playerTwoCount++)
 
         }
-        moves++
-
+        // Count Total moves and end game if more than 8
+        if (moves == 9) {
+            console.log("Game Over")
+        }
+        else {
+            console.log("moves = " + moves++)
+        }
 
     });
-    // }
-    // gamePlay(p1, p2)
-
-
-    // let val, h1;
-    // //getting the the entered text
-    // val = $("input").val();
-    // paragraph = $("p")
-    // paragraph.text(val); // the value of the entered text is the new value of h1 tag
-
-
-    // }
-
-
-    // let players = function() {
-
-    // }
-    // players turns 
-
 
 
     // let restartGame = function() {
-    if (moves == 8) {
-        console.log("Game Over")
-    }
-    else {
-        console.log("Game still on") // place holder
-    }
+
+
+
     // go back to the main function
 
     // }
 
 
-    // let theWinner = function() {
-    // const win = [
-    //     [1, 2, 3], [4, 5, 6], [7, 8, 9],
-    //     [1, 4, 7], [2, 5, 8], [3, 6, 9],
-    //     [1, 5, 9], [3, 5, 7],
-
-    //     // return the winner    
-    // ]
-
-    function whoseMove(lastPlayer, win) {
-        return lastPlayer == "white" ^ !!win ? "black" : "white";
-    }
     // for (let i = 0; i<3; i++) {
     //     if (grid[i][0] !== " " && )
     // }
