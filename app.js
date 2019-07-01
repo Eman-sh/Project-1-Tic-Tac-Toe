603ac806727397d4d74e29e1708c3702604c7541
Eman-sh <amoooony@hotmail.com>
2019-07-01 10:01:55 +0300

    1st winnig condetion

--- app.js
+++ app.js
@@ -1,61 +1,62 @@
 $(document).ready(function () {
-//     let grid = [
-//         [" ", " ", " "],
-//         [" ", " ", " "],
-//        [" ", " ", " "]
-//    ];
+    //     let grid = [
+    //         [" ", " ", " "],
+    //         [" ", " ", " "],
+    //        [" ", " ", " "]
+    //    ];
     let playerOne = "X";
     let playerTwo = "O";
     let playerOneCount = 1;
     let playerTwoCount = 1;
-    let moves = 1;
+    let total_moves = 0;
     let players;
     let p1;
     let p2;
+    let column = $(".column");
 
 
     // 2 players take turns in playing
-    const gamePlay = $(".column").one("click", function () {
+    const gamePlay = column.one("click", function () {
         // players.push($(this).attr('class'));
 
 
         if (playerOneCount <= playerTwoCount) {
             p1 = $(this).text(playerOne)
-      
 
 
             // Count player moves
-            console.log("player1 = " + playerOneCount++)
-
+            console.log(("total_moves = " + total_moves++)+" player1 = " + playerOneCount++)
+            
 
         } else {
             p2 = $(this).text(playerTwo)
             // Count player moves
-            console.log("player2 = " + playerTwoCount++)
+            console.log(("total_moves = " + total_moves++)+ " player2 = " + playerTwoCount++ )
 
-                }
-              
+        }
+         });
 
         // Count Total moves and end game if more than 8
-        if (moves == 9) {
-            
+        if ($(".column")[0].innerHTML === $(".column")[1].innerHTML || $(".column")[2].innerHTML) {
+            return true;
+        }
+        else if (total_moves == 9) {
+
             console.log("Game Over")
         }
-        else {
-            console.log("moves = " + moves++)
-        }
 
-    });
+
+   
     //innerHTML
     //textContent
 
     //////////////////////////////////////
-    const winner = function () {
-        const win = [[0, 1, 2], [4, 5, 6], [7, 8, 9],
-        [1, 4, 7], [2, 5, 8], [3, 6, 9],
-        [1, 5, 9], [3, 5, 7],]; // return the winner 
+    // const winner = function () {
+    //     const win = [[0, 1, 2], [4, 5, 6], [7, 8, 9],
+    //     [1, 4, 7], [2, 5, 8], [3, 6, 9],
+    //     [1, 5, 9], [3, 5, 7],]; // return the winner 
 
-    }
+    // }
 
 
 
@@ -65,7 +66,7 @@ $(document).ready(function () {
         location.reload();
     });
 
-    
+
 });
 
     // for (let i = 0; i<3; i++) {
